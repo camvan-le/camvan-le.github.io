4b309086c0945687393c0ebc634e3f9c27ef699b
camvan-le <camvan.le@torontomu.ca>
2024-10-09 14:25:41 -0400

    update

--- lab5.js
+++ lab5.js
@@ -1,29 +1,41 @@
 function nameChange(){
     const displayUserName = document.getElementById("displayUserName");
     const inputName = document.getElementById("inputName").value;
-    displayUserName.innerText = `Hello, ${inputName}`;
+    displayUserName.innerText = `Hello, ${inputName}!`;
+}
+
+// Function to apply the hover effect
+function hoverEffect(element) {
+    element.style.color = '#e4c5c2'; // Change text color to gold
+    console.log("abc")
+}
+
+// Function to reset the effect when mouse leaves
+function resetEffect(element) {
+    element.style.backgroundColor = ''; // Reset background color
+    element.style.color = ''; // Reset text color
 }
 
 function toggleZoom() {
     const img = document.getElementById("img");
 
     if (img.style.height === '400px' && img.style.width === '400px'){
-        img.style.height = '260px';
-        img.style.width = '260px'
+        img.style.height = '320px';
+        img.style.width = '320px'
     } else {
         img.style.width = "400px";
         img.style.height = "400px";
-    }
-    
-}
+    }}
 
-const btn = document.getElementById('button');
-const blue = 'Blue';
-const red = 'Red'
-btn.addEventListener('mouseover', function(){
-    btn.style.backgroundColor = red;
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const button = document.getElementById('button')
 
-btn.addEventListener('mouseout', function(){
-    btn.style.backgroundColor = blue;
-});
+    button.addEventListener('mouseover', function() {
+        button.style.backgroundColor = '#13315c'
+    }, true);
+
+    button.addEventListener('mouseout', function() {
+        button.style.backgroundColor = ''
+    }, true);
+}); 
+
